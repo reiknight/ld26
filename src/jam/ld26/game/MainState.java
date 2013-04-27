b6e0946828f71c1fe0ae19ebf52d4a61f757715e
Reik Val <guardian_de_las_sombras@hotmail.com>
2013-04-27 19:28:58 +0200

    Quitados algunos imports innecesarios

--- src/jam/ld26/game/MainState.java
+++ src/jam/ld26/game/MainState.java
@@ -7,8 +7,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.state.transition.FadeInTransition;
-import org.newdawn.slick.state.transition.FadeOutTransition;
 
 public class MainState extends ManagedGameState {
     private boolean paused = false;
